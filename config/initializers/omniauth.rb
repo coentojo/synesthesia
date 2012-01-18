56369a3ab0c61367cf71d2d8f062124ddbfc6f8e
Chris Oentojo <c.oentojo@gmail.com>
2012-01-17 21:52:14 -0800

    Changed oauth cred

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 #Production
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#   provider :facebook, '333859243294743', 'e32e626336c6f9dff311d3ae89609b96'
-# end
-#dev
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '302401596450772', '24d20ad30724e75a6695368171fa809f'
+  provider :facebook, '333859243294743', 'e32e626336c6f9dff311d3ae89609b96'
 end
+#dev
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :facebook, '302401596450772', '24d20ad30724e75a6695368171fa809f'
+# end
